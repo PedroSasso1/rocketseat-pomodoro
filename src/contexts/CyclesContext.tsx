572e5824f3cdfd6ab3c3c5b396b6bd094d116a0f
pedrosasso1 <pedrosassom@outlook.com>
2024-07-08 01:36:24 -0300

    cycles reducer

---
+++ src/contexts/CyclesContext.tsx
@@ -0,0 +1,89 @@
+import { createContext, ReactNode, useState, useReducer } from 'react'
+import { ActionTypes, Cycle, cyclesReducer } from '../reducers/cycles/reducer'
+import {
+  addNewCycleAction,
+  interruptCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from '../reducers/cycles/actions'
+
+interface CreateCycleData {
+  task: string
+  minutesAmount: number
+}
+
+interface CyclesContextData {
+  cycles: Cycle[]
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  amountSecondsPassed: number
+  setSecondsPassed: (seconds: number) => void
+  markCurrentCycleAsFinished: () => void
+  createNewCycle: (data: CreateCycleData) => void
+  interruptCurrentCycle: () => void
+}
+
+export const CyclesContext = createContext({} as CyclesContextData)
+
+interface CyclesContextProviderProps {
+  children: ReactNode
+}
+
+export function CyclesContextProvider({
+  children,
+}: CyclesContextProviderProps) {
+  const [cycleState, dispatch] = useReducer(cyclesReducer, {
+    cycles: [],
+    activeCycleId: null,
+  })
+
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  const activeCycle = cycleState.cycles.find(
+    (cycle) => cycle.id === activeCycleId,
+  )
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
+
+  function createNewCycle(data: CreateCycleData) {
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    }
+    dispatch(addNewCycleAction(newCycle))
+    setAmountSecondsPassed(0)
+  }
+
+  function markCurrentCycleAsFinished() {
+    dispatch(markCurrentCycleAsFinishedAction())
+    setActiveCycleId(null)
+    setAmountSecondsPassed(0)
+  }
+
+  function interruptCurrentCycle() {
+    dispatch(interruptCurrentCycleAction())
+    setActiveCycleId(null)
+    setAmountSecondsPassed(0)
+  }
+
+  return (
+    <CyclesContext.Provider
+      value={{
+        cycles: cycleState.cycles,
+        activeCycle,
+        activeCycleId,
+        amountSecondsPassed,
+        setSecondsPassed,
+        markCurrentCycleAsFinished,
+        createNewCycle,
+        interruptCurrentCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  )
+}
